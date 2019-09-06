977b68863370c69cbe1a3a3487bb9935e5bcb4f4
Gianni Di Noia <giovanni.dinoia@gig.com>
2019-09-06 10:23:33 +0200

    switch to python3

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pip install -r requirements.txt
-pip install -r requirements-test.txt
+python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements-test.txt
 pytest
