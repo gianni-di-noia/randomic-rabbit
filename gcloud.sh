21b5ab973d4811a35952aa44b67f2466317247bc
Gianni Di Noia <giovanni.dinoia@gig.com>
2019-09-06 10:53:45 +0200

    fix python version

--- gcloud.sh
+++ gcloud.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-docker build --rm -f "Dockerfile" -t randomic_rubbit:latest .
-docker tag randomic_rubbit gcr.io/cdn-dinoia/randomic_rubbit:latest
+# docker build --rm -f "Dockerfile" -t randomic_rubbit:latest .
+# docker tag randomic_rubbit gcr.io/cdn-dinoia/randomic_rubbit:latest
 docker push gcr.io/cdn-dinoia/randomic_rubbit:latest
-
 # gcloud compute instance-groups managed rolling-action restart workers \
 #     --max-unavailable 100%
 
-aplay /usr/share/sounds/purple/alert.wav
-date +"%T"
+# aplay /usr/share/sounds/purple/alert.wav
+# date +"%T"
